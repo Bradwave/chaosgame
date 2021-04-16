402bdcf45352074445a94a9069579145c95947e4
Matteo Bramardi <bradwave@users.noreply.github.com>
2021-04-16 22:34:39 +0200

    Add minimal UI

--- js/chaosgame.js
+++ js/chaosgame.js
@@ -1,79 +1,125 @@
 /**
  * Geometry
  */
-let xOrigin;
-let yOrigin;
+let cartesianOrigin;
 
-let scaleFactor = 100.000000;
-let descaleFactor = 1.000000 / scaleFactor;
+let pixelsPerUnit = 100.000000;
+let unitsPerPixel = 1.000000 / pixelsPerUnit;
 
 /**
  * Main figure
  */
-const numberOfPoints = 5;
+let numberOfPoints = 5;
 let points = [];
 
 /**
  * Chaos factor
  */
-const chaosFactor = 0.5;
+let chaosFactor = 0.5;
 
 /**
  * Starting point
  */
-let p;
+let newPoint;
 
 /**
  * Rules
  */
 let previousIndexes = [];
-const preventPrevious = 0;
-const startEqual = 0;
-const endEqual = 2;
-const compareIndex = 0;
-const removedDistance = 1;
-const ignoreRemoved = Math.abs(endEqual - startEqual) == 1;
+let preventPrevious = 0;
+let startEqual = 0;
+let endEqual = 2;
+let compareIndex = 0;
+let removedDistance = 1;
+let ignoreRemoved = Math.abs(endEqual - startEqual) == 1;
 
 /**
  * Style
  */
-const pointWidth = .1;
-const drawingSpeed = 500;
+let pointWidth = .1;
+let drawingSpeed = 500;
 
-let iterationCounter = 0;
+let iterations = 1000;
+let iterationCounter = iterations;
+
+/**
+ * HTML elements
+ */
+let htmlElements;
+
+const convertValueInt = (givenValue, minValue, maxValue, defaultValue, id) => {
+  givenValue = Math.round(parseInt(givenValue));
+  givenValue = isNaN(givenValue) ? defaultValue : (
+    givenValue < minValue ? minValue : (
+      givenValue > maxValue ? maxValue : givenValue
+    )
+  );
+  document.getElementById(id).value = givenValue;
+  return givenValue;
+}
+
+const convertValueFloat = (givenValue, minValue, maxValue, defaultValue, id) => {
+  givenValue = parseFloat(givenValue);
+  givenValue = isNaN(givenValue) ? defaultValue : (
+    givenValue < minValue ? minValue : (
+      givenValue > maxValue ? maxValue : givenValue
+    )
+  );
+  document.getElementById(id).value = givenValue;
+  return givenValue;
+}
+
+function changeFigure(num) {
+  numberOfPoints = convertValueInt(num, 3, 12, 0, "vertices");
+
+  createPolygon(numberOfPoints);
+  resetRender();
+}
+
+function changeRules(newValue, id) {
+  switch (id) {
+    case "preventPrevious":
+      preventPrevious = convertValueInt(newValue, 0, 12, 0, id);
+      break;
+    case "endEqual":
+      endEqual = convertValueInt(newValue, 1, 12, 2, id);
+      break;
+    case "removedDistance":
+      removedDistance = convertValueInt(newValue, 1, 12, 1, id);
+      break;
+    case "compareIndex":
+      compareIndex = convertValueInt(newValue, 1, 12, 1, id) - 1;
+      break;
+    case "chaosFactor":
+      chaosFactor = convertValueFloat(newValue, 0.01, 0.99, 0.5, id)
+  }
+  ignoreRemoved = Math.abs(endEqual - startEqual) == 1;
+
+  resetRender();
+}
+
+function changeStyle(newValue, id) {
+  switch (id) {
+    case "pointWidth":
+      pointWidth = convertValueFloat(newValue, 0.05, 10, 0.1, id);
+      break;
+    case "iterations":
+      iterations = convertValueInt(newValue, 1, 100000, 1000, id);
+      break;
+    case "drawingSpeed":
+      drawingSpeed = convertValueInt(newValue, 1, 10000, 500, id)
+      break;
+    default:
+  }
+  
+  resetRender();
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  // noLoop();
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("main");
 
   resetView();
-}
-
-function createFigure(numberOfPoints) {
-  points = [];
-  for (let i = 0; i < numberOfPoints; i++) {
-    const angle = i / numberOfPoints * TWO_PI + ((numberOfPoints % 2 == 0) ? PI / (numberOfPoints) : HALF_PI);
-    const figurePoint = createVector(Math.cos(angle), Math.sin(angle));
-    points.push(toScreenCoordinates(figurePoint));
-  }
-}
-
-function resetView() {
-  setOrigin();
-
-  p = createVector(width / 2, height / 2);
-  createFigure(numberOfPoints);
-
-  background(0);
-
-  stroke(255);
-  strokeWeight(5);
-  points.forEach(p => point(p));
-
-  strokeWeight(pointWidth);
-
-  iterationCounter = 0;
 }
 
 function windowResized() {
@@ -83,26 +129,63 @@ function windowResized() {
   resetView();
 }
 
-function setOrigin() {
+function resetView() {
+  resetGeometry();
+  createPolygon(numberOfPoints);
+  resetRender();
+}
+
+function resetGeometry() {
   let minDim = min(width, height);
 
-  xOrigin = width * 0.5;
-  yOrigin = height * 0.5;
+  cartesianOrigin = {
+    x: width * 0.5,
+    y: height * 0.5,
+  };
 
-  scaleFactor = minDim / 2.2;
-  descaleFactor = 1.000000 / scaleFactor;
+  pixelsPerUnit = minDim / 2.2;
+  unitsPerPixel = 1.000000 / pixelsPerUnit;
+}
+
+function createPolygon(numberOfPoints) {
+  points = [];
+  for (let i = 0; i < numberOfPoints; i++) {
+    const angle = i / numberOfPoints * TWO_PI + ((numberOfPoints % 2 == 0) ? PI / (numberOfPoints) : HALF_PI);
+    points.push(toScreenCoordinates({
+      x: Math.cos(angle),
+      y: Math.sin(angle)
+    }));
+  }
+}
+
+function resetRender() {
+  background(0);
+
+  /* Draw main polygon */
+  stroke(255);
+  strokeWeight(5);
+  points.forEach(p => point(p.x, p.y));
+
+  /* Draw first point */
+  strokeWeight(pointWidth);
+  newPoint = createVector(width * 0.5, height * 0.5);
+
+  iterationCounter = iterations;
+  loop();
 }
 
 function toCartesian(p) {
-  let cx = (p.x - xOrigin) * descaleFactor;
-  let cy = (yOrigin - p.y) * descaleFactor;
-  return createVector(cx, cy);
+  return {
+    x: (p.x - cartesianOrigin.x) * unitsPerPixel,
+    y: (cartesianOrigin.y - p.y) * unitsPerPixel
+  }
 }
 
 function toScreenCoordinates(p) {
-  let sx = p.x * scaleFactor + xOrigin;
-  let sy = yOrigin - p.y * scaleFactor;
-  return createVector(sx, sy);
+  return {
+    x: p.x * pixelsPerUnit + cartesianOrigin.x,
+    y: cartesianOrigin.y - p.y * pixelsPerUnit
+  }
 }
 
 const calcNewCoordinate = (c1, c2) => {
@@ -111,11 +194,11 @@ const calcNewCoordinate = (c1, c2) => {
 
 const calcNewPoint = () => {
   const chosenPoint = points[chooseRandomIndex()];
-  p = createVector(
-    calcNewCoordinate(p.x, chosenPoint.x),
-    calcNewCoordinate(p.y, chosenPoint.y)
+  newPoint = createVector(
+    calcNewCoordinate(newPoint.x, chosenPoint.x),
+    calcNewCoordinate(newPoint.y, chosenPoint.y)
   );
-  return p;
+  return newPoint;
 }
 
 const removedIndexes = () => {
@@ -150,13 +233,8 @@ const chooseRandomIndex = () => {
   return newIndex;
 }
 
-function drawNewPoint() {
-  point(calcNewPoint());
-}
-
 function draw() {
-  [...Array(drawingSpeed)].forEach(() => drawNewPoint())
-
-  if (iterationCounter++ > 1000)
-    noLoop();
+  let j = drawingSpeed;
+  while (j-- > 0) point(calcNewPoint())
+  if (iterationCounter-- < 1) noLoop();
 }
